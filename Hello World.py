a99fce45a74b2cd08352462dacf1abc6899505bd
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-06-10 16:28:15 -0500

    Again!

--- Hello World.py
+++ Hello World.py
@@ -1,6 +1,6 @@
 def message():
-	print("Hello World")
+    print("Hello World")
 
 
 if __name__ == '__main__':
-	message()
+    message()
--- triangulo.py
+++ triangulo.py
@@ -1,11 +1,19 @@
 def triangulo():
-	cantidad = int(input("Ingrese la cantidad de lineas: "))
-	
-	for i in range(1,cantidad):
-		for j in range (0,i):
-			print("x",end="")
-		print("\n")
+
+    lado1 = int(input("Ingrese el lado 1: "))
+    lado2 = int(input("Ingrese el lado 2: "))
+    lado3 = int(input("Ingrese el lado 3: "))    
+
+    if (lado1 < (lado2+lado3)):
+        print("Es un triángulo")
+    elif (lado2< (lado1+lado3)):
+        print("Es un triángulo")
+    elif (lado3 < (lado1+lado2)):
+        print("Es un triángulo")
+
+    if((lado1 == lado2) and (lado1 == lado3) and (lado2==lado3)):
+        print("Es un triángulo equilátero")
 
 
 if __name__ == '__main__':
-	triangulo()
+    triangulo()
