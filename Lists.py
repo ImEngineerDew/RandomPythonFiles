187bf8ebc23eaf357c9271eaf8bfa0513f929946
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-06-10 22:45:41 -0500

    I did forget to add those files

---
+++ Lists.py
@@ -0,0 +1,10 @@
+def Lists():
+
+    while True:
+        España= []
+        España.append(input("Ingrese una comunidad autónoma: "))
+        print(España)
+
+
+if __name__ == '__main__':
+    Lists()
