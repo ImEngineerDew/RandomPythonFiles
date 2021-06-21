749746066a9fc60751e41060984cb7d35566fca5
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-06-21 17:22:49 -0500

    I am practicing about of the Python functions

---
+++ CalculadoraFunciones.py
@@ -0,0 +1,37 @@
+def suma (a,b):
+    return a+b
+
+def resta(a,b):
+    return a-b
+
+def producto(a,b):
+    return a*b
+
+
+def calculadora():
+
+    opcion = int(input("Ingrese una opción: "))
+
+    if(opcion == 1):
+        opc_a = int(input("Ingrese el valor de a: "))
+        opc_b = int(input("Ingrese el valor de b: "))
+        resultado = suma(opc_a,opc_b)
+
+        print("El resultado de la suma es: "+str(resultado))
+    elif (opcion == 2):
+        opc_a = int(input("Ingrese el valor de a: "))
+        opc_b = int(input("Ingrese el valor de b: "))
+        resultado = resta(opc_a,opc_b)
+
+        print("El resultado de la resta es: "+str(resultado))
+    elif (opcion == 3):
+        opc_a = int(input("Ingrese el valor de a: "))
+        opc_b = int(input("Ingrese el valor de b: "))
+        resultado = producto(opc_a,opc_b)
+
+        print("El resultado del producto es igual a: "+str(resultado))
+
+
+
+if __name__ == '__main__':
+    calculadora()
--- Lista_Camioneros.py
+++ Lista_Camioneros.py
@@ -2,43 +2,51 @@ def matrix():
     nombres = []
     apellidos = [] 
     week = []
-    sumatory = []
+    sumatory = []    
 
-    #The quantity of the elements that may be introduced by keyboard
-    quantity = int(input("Please write the quantity of data to input (write 0 to exit): "))  
+    try:
+        #The quantity of the elements that may be introduced by keyboard
+        quantity = int(input("Please write the quantity of data to input (write 0 to exit): "))     
+       
+        while True:
+            if (quantity !=0):        
+                for i in range(quantity):
+                    ingresar_nombre = input("Type your name: ")
+                    ingresar_apellido = input("Type your surname: ")
 
-    while True:
-        if (quantity >0):        
-            for i in range(quantity):
-                ingresar_nombre = input("Type your name: ")
-                ingresar_apellido = input("Type your surname: ")
+                    #Space for adding names and surnames
+                    nombres.append(ingresar_nombre)
+                    apellidos.append(ingresar_apellido)
 
-                #Space for adding names and surnames
-                nombres.append(ingresar_nombre)
-                apellidos.append(ingresar_apellido)
-
-                monday     = int(input("km traveled on monday: "))
-                tuesday    = int(input("km traveled on tuesday: "))
-                wednesday  = int(input("km traveled on wednesday: "))
-                thrusday   = int(input("km traveled on thrusday: "))
-                friday     = int(input("km traveled on friday: "))
-                saturday   = int(input("km traveled on saturday: "))
-                sunday     = int(input("km traveled on sunday: "))   
+                    try:
+                        monday     = int(input("km traveled on monday: "))
+                        tuesday    = int(input("km traveled on tuesday: "))
+                        wednesday  = int(input("km traveled on wednesday: "))
+                        thrusday   = int(input("km traveled on thrusday: "))
+                        friday     = int(input("km traveled on friday: "))
+                        saturday   = int(input("km traveled on saturday: "))
+                        sunday     = int(input("km traveled on sunday: "))   
         
-                #Space for adding the kilometers per hoy traveled by day
-                week.append([monday,tuesday,wednesday,thrusday,friday,saturday,sunday])
+                        #Space for adding the kilometers per hoy traveled by day
+                        week.append([monday,tuesday,wednesday,thrusday,friday,saturday,sunday])
             
-                #This line makes the sum of the days traveled on km
-                suma_semanas = monday+tuesday+wednesday+thrusday+friday+saturday+sunday
+                        #This line makes the sum of the days traveled on km
+                        suma_semanas = monday+tuesday+wednesday+thrusday+friday+saturday+sunday
 
-                sumatory.append(suma_semanas)
+                        sumatory.append(suma_semanas)
+                        
+                        for i,j,l in zip(nombres,apellidos,sumatory):
+                            print(i,j," = ",l) 
 
-                for i,j,k,l in zip(nombres,apellidos,week,sumatory):
-                    print(i,j,"\t",k,"=",l)  
-            break
-        else:
-            print("End of the program!")
-            break
+                    except (ValueError,UnboundLocalError):
+                        print("You've had written a string, as well as that the append is out!")
+                break
+            else:
+                print("End of the program!")
+                break
+
+    except ValueError:
+        print("You've written the string chain!")
   
 if __name__ == '__main__':
     matrix()
