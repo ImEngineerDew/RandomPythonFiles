826e2001458f09e121e927b3aeddcbd0deaa63ba
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-06-28 18:15:19 -0500

    Making upgrades to the test 4

--- Clientes.py
+++ Clientes.py
@@ -1,51 +1,57 @@
-def add_client():  
+main_list = {}
 
-    global main_list,personal_list
+def add_client():
+    global main_list
 
     while True:
-        name    = input("Ingrese su nombre, por favor: ")
         DNI     = int(input("Ingrese su DNI, por favor: "))
+        name    = input("Ingrese su nombre, por favor: ")        
         address = input("Ingrese su dirección, por favor: ") 
         phone   = int(input("Ingrese su número de teléfono, por favor: "))
         mail    = input("Ingrese su correo por favor: ")
-        preferences = input("¿Su cliente es preferente: (Y/N)?").upper()    
-        show_preferences(preferences)    
-
+        preferences = input("¿Su cliente es preferente: (Y/N)?").upper()
+        
+        #if preferences == 'Y': 
+        #    return True
+        #elif preferences == 'N':
+        #    return False          
+        personal_list ={} 
+        
         personal_list["Nombre"] = name
         personal_list["DNI"] = DNI
         personal_list["Dir"] = address
         personal_list["Tel"] = phone
         personal_list["email"] = mail
-        personal_list["Preferente"] = preference
+        personal_list["Preferente"] = preferences
 
         main_list[DNI] = personal_list
+        break
 
-        opc = input("Desea agregar otro dato (SI/NO): ").upper()
 
-        if(opc == 'NO'):
-            break 
-
-def erase_client():
-    pass
-
-def show_unique_client():
-    pass
+def erase_client(DNI):
+    main_list.pop(DNI)
+    print("Entrada eliminada")    
+    
+def show_unique_client(DNI):
+    print(main_list.get(DNI))   
 
 def show_all_clients():
-    global main_list
-    print(main_list)
+    for i, j in main_list.items():
+        print(i,"=",j)
 
-def show_preferences(preference):
-    if (preference == 'Y'):
+def show_preferences(preferences):
+     if preferences == 'Y':
+        print (main_list.get(preferences))
         return True
-    elif (preference == 'N'):
-        return False
-
+     elif preferences == 'N':
+        return False  
+    
+  
 def menu():
 
     while True:
 
-        print("""--------Listado de opciones------------
+        print("""--------Listado de opciones--------
                 1. Añadir cliente
                 2. Borrar un cliente
                 3. Mostrar un cliente
@@ -58,21 +64,21 @@ def menu():
         if(opcion == 1):
             add_client()           
         elif(opcion == 2):
-            erase_client()
+            CC= int(input("Ingrese su DNI, por favor: "))
+            erase_client(CC)
         elif(opcion == 3):
-            show_unique_client()
+            CC = int(input("Ingrese su DNI, por favor: "))
+            show_unique_client(CC)
         elif (opcion == 4):
             show_all_clients()
         elif (opcion == 5):
-            show_preferences()
+            preferente = input("¿El cliente es preferente: ?").upper()
+            show_preferences(preferente)
         elif(opcion == 6):
             print("Fin del programa!")
             return False
             break
         
 
-if __name__ == '__main__':
-
-    main_list = { }
-    personal_list ={ }
+if __name__ == '__main__':   
     menu()
