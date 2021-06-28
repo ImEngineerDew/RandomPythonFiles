caaa8ca2aa7dc5654fd7164f5c3ebaee375a2e16
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-06-28 18:38:41 -0500

    A new upgrade to the test 4

--- Clientes.py
+++ Clientes.py
@@ -4,28 +4,27 @@ def add_client():
     global main_list
 
     while True:
-        DNI     = int(input("Ingrese su DNI, por favor: "))
-        name    = input("Ingrese su nombre, por favor: ")        
-        address = input("Ingrese su dirección, por favor: ") 
-        phone   = int(input("Ingrese su número de teléfono, por favor: "))
-        mail    = input("Ingrese su correo por favor: ")
-        preferences = input("¿Su cliente es preferente: (Y/N)?").upper()
+        try:
+            DNI     = int(input("Ingrese su DNI, por favor: "))
+            name    = input("Ingrese su nombre, por favor: ")        
+            address = input("Ingrese su dirección, por favor: ") 
+            phone   = int(input("Ingrese su número de teléfono, por favor: "))
+            mail    = input("Ingrese su correo por favor: ")
+            preferences = input("¿Su cliente es preferente: (Y/N)?").upper()      
+                
+            personal_list ={} 
         
-        #if preferences == 'Y': 
-        #    return True
-        #elif preferences == 'N':
-        #    return False          
-        personal_list ={} 
-        
-        personal_list["Nombre"] = name
-        personal_list["DNI"] = DNI
-        personal_list["Dir"] = address
-        personal_list["Tel"] = phone
-        personal_list["email"] = mail
-        personal_list["Preferente"] = preferences
+            personal_list["Nombre"] = name
+            personal_list["DNI"] = DNI
+            personal_list["Dir"] = address
+            personal_list["Tel"] = phone
+            personal_list["email"] = mail
+            personal_list["Preferente"] = preferences
 
-        main_list[DNI] = personal_list
-        break
+            main_list[DNI] = personal_list
+            break
+        except ValueError:
+            print("¡Por favor intente de nuevo!")
 
 
 def erase_client(DNI):
@@ -44,13 +43,11 @@ def show_preferences(preferences):
         print (main_list.get(preferences))
         return True
      elif preferences == 'N':
-        return False  
-    
+        return False      
   
 def menu():
 
     while True:
-
         print("""--------Listado de opciones--------
                 1. Añadir cliente
                 2. Borrar un cliente
@@ -58,26 +55,29 @@ def menu():
                 4. Mostrar todos los clientes 
                 5. Mostrar sólo los clientes preferentes
                 6. Finalizar el programa""")
+
+        try:
+            opcion = int(input("Seleccione una opción de la lista: "))   
     
-        opcion = int(input("Seleccione una opción de la lista: "))   
-    
-        if(opcion == 1):
-            add_client()           
-        elif(opcion == 2):
-            CC= int(input("Ingrese su DNI, por favor: "))
-            erase_client(CC)
-        elif(opcion == 3):
-            CC = int(input("Ingrese su DNI, por favor: "))
-            show_unique_client(CC)
-        elif (opcion == 4):
-            show_all_clients()
-        elif (opcion == 5):
-            preferente = input("¿El cliente es preferente: ?").upper()
-            show_preferences(preferente)
-        elif(opcion == 6):
-            print("Fin del programa!")
-            return False
-            break
+            if(opcion == 1):
+                add_client()           
+            elif(opcion == 2):
+                CC= int(input("Ingrese su DNI, por favor: "))
+                erase_client(CC)
+            elif(opcion == 3):
+                CC = int(input("Ingrese su DNI, por favor: "))
+                show_unique_client(CC)
+            elif (opcion == 4):
+                show_all_clients()
+            elif (opcion == 5):
+                preferente = input("¿El cliente es preferente: ?").upper()
+                show_preferences(preferente)
+            elif(opcion == 6):
+                print("Fin del programa!")
+                return False
+                break
+        except ValueError:
+            print("¡Por favor intente de nuevo!")
         
 
 if __name__ == '__main__':   
