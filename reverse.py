ae72575270cecbb7ecec6c3fff36424b0bfdfa29
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-10-27 17:37:36 -0500

    Palindrome done!

--- reverse.py
+++ reverse.py
@@ -1,9 +1,13 @@
+
 def reverse(text):
-	text[::-1]
-	print(text)
+     return text[::-1]
+	
 
 if __name__ == '__main__':
-     phrase =  input("Please type your text or phrase: ")
+     phrase =  input("Please type your text or phrase: ").lower().join('')  
      backwards = reverse(phrase)
-     print(backwards)
-	
+
+     if phrase == backwards:
+          print("It's a palindrome")
+     else:
+          print("This is not a palindrome")
