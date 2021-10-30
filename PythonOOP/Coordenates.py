005c0c2b2df3642ff52ec81d44ea544626d1865c
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-10-29 21:48:04 -0500

    Moving the files

---
+++ PythonOOP/Coordenates.py
@@ -0,0 +1,17 @@
+class Coordenates:
+    def __init__ (self,x,y):
+        self.x = x
+        self.y = y
+    
+    def distance(self, otraCoordenada):
+        x_diff = (self.x - otraCoordenada.x)**2
+        y_diff = (self.y - otraCoordenada.y)**2
+
+        return (x_diff+y_diff)**0.5
+
+if __name__ == '__main__':
+    coord1 = Coordenates(3,30)
+    coord2 = Coordenates(4,8)
+
+    #print(coord1.distance(coord2))
+    print(isinstance(coord2,Coordenates))
